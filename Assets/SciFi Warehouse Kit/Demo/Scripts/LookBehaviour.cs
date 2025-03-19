b8532b891e8cfbea773dfac2e8301bf3187068c2
Rohan <anakrv1@student.op.ac.nz>
2025-03-20 10:28:02 +1300

    refactor: rework move and look to use PlayerInput

---
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/LookBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LookBehaviour : MonoBehaviour
+{
+    public float lookSensitivityX = 100f;
+    public float lookSensitivityY = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    Vector2 lookVector;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lookX = lookVector.x * lookSensitivityX * Time.deltaTime;
+        float lookY = lookVector.y * lookSensitivityY * Time.deltaTime;
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * lookX);
+    }
+
+    public void OnLook (InputAction.CallbackContext context)
+    {
+        lookVector = context.ReadValue<Vector2>();
+    }
+}
--- Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -1,60 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header("Movement"), Tooltip("Properties for movement")]
     public CharacterController controller;
-    public float speed = 8f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public const float speed = 8f;
+    public const float gravity = -9.81f;
+    Vector3 velocity;
+    Vector2 moveAmount;
+
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    Vector3 velocity;
     bool isGrounded;
 
-    
-     public AudioClip footStepSound;
-     public float footStepDelay;
- 
-     private float nextFootstep = 0;
+    public AudioClip footStepSound;
+    public float footStepDelay;
+    private float nextFootstep = 0;
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);    
+    }
+
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y <0)
-            {
-            velocity.y = -2f;
-            }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 motion = transform.right * x + transform.forward * z;
-        controller.Move(motion * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        Vector3 motion = transform.right * moveAmount.x + transform.forward * moveAmount.y;
+        controller.Move(motion.normalized * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
+        if (moveAmount.x > 0 || moveAmount.y > 0 )
+        {
+            nextFootstep -= Time.deltaTime;
+            if (nextFootstep <= 0)
             {
-             nextFootstep -= Time.deltaTime;
-             if (nextFootstep <= 0) 
-                {
-                 GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
-                 nextFootstep += footStepDelay;
-                }
-             }
-         }
+                GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+                nextFootstep += footStepDelay;
+            }
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveAmount = context.ReadValue<Vector2>();
+    }
 }
 
 
