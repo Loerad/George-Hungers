a2e0c18f43f00e9259da7bf01bf40d86d4cc40c3
Rohan <anakrv1@student.op.ac.nz>
2025-03-20 14:33:07 +1300

    refactor: sanitize code

--- Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -5,29 +5,33 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement"), Tooltip("Properties for movement")]
-    
-    public CharacterController controller;
+    [SerializeField]
+    private CharacterController controller;
     private float speed = 5f;
-    private const float sprintValue = 3f;
-    private const float gravity = -9.81f;
-    Vector3 velocity;
-    Vector2 moveAmount;
+    private const float SPRINT_VALUE = 3f;
+    private const float GRAVITY = -9.81f;
+    private Vector3 velocity;
+    private Vector2 moveAmount;
 
-    [Header("GroundCheck"), Tooltip("Properties for ground check, may be removed")]
-    
-    public Transform groundCheck;
-    private float groundDistance = 0.45f;
-    public LayerMask groundMask;
+    [Header("GroundCheck"), Tooltip("Properties for ground check")]
+    [SerializeField]
+    private Transform groundCheck;
+    private const float GROUND_DISTANCE = 0.45f;
+    [SerializeField]
+    private LayerMask groundMask;
     bool isGrounded;
 
     [Header("Audio"), Tooltip("Properties for footsteps")]
     
     [SerializeField]
     private GroundFlavour groundFlavour;
+    private const int METAL_LAYER = 3;
     private AudioSource audioSource;
     
     [SerializeField]
     private AudioClip footStepSound;
+    private const float WALKING_DELAY = 0.42f;
+    private const float SPRINTING_DELAY = 0.35f;
     private float footStepDelay = 0.42f;
     private float nextFootstep = 0;
     
@@ -44,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);    
+        isGrounded = Physics.CheckSphere(groundCheck.position, GROUND_DISTANCE, groundMask);    
     }
 
     void Update()
@@ -52,15 +56,40 @@ public class PlayerMovement : MonoBehaviour
         Vector3 motion = transform.right * moveAmount.x + transform.forward * moveAmount.y;
         controller.Move(motion.normalized * speed * Time.deltaTime);
 
-        velocity.y += gravity * Time.deltaTime;
+        velocity.y += GRAVITY * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-        if (moveAmount.x != 0 || moveAmount.y != 0 )
+        HandleStepSound();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) //unity event check the Player Input Component for context
+    {
+        moveAmount = context.ReadValue<Vector2>();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            speed += SPRINT_VALUE;
+            footStepDelay = SPRINTING_DELAY;
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            speed -= SPRINT_VALUE;
+            footStepDelay = WALKING_DELAY;
+        }
+        
+    }
+
+    private void HandleStepSound()
+    {
+        if (moveAmount.x != 0 || moveAmount.y != 0 ) //step behaviour 
         {
             nextFootstep -= Time.deltaTime;
             if (nextFootstep <= 0)
             {
-                if (groundFlavour.CurrentFloor() == 3) //metal layer
+                if (groundFlavour.CurrentFloor() == METAL_LAYER)
                 {
                     RandomizeStep(metalFootsteps);
                 }
@@ -68,7 +97,7 @@ public class PlayerMovement : MonoBehaviour
                 {
                     RandomizeStep(concreteFootsteps);
                 }
-                audioSource.PlayOneShot(footStepSound, 0.5f);
+                audioSource.PlayOneShot(footStepSound, 0.5f); //arbitrary volume 
                 nextFootstep += footStepDelay;
             }
         }
@@ -78,26 +107,6 @@ public class PlayerMovement : MonoBehaviour
     {
         int nextSound = Random.Range(0, steps.Count - 1);
         footStepSound = steps[nextSound];
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveAmount = context.ReadValue<Vector2>();
-    }
-
-    public void OnSprint(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Started)
-        {
-            speed += sprintValue;
-            footStepDelay = 0.35f;
-        }
-        else if (context.phase == InputActionPhase.Canceled)
-        {
-            speed -= sprintValue;
-            footStepDelay = 0.42f;
-        }
-        
     }
 }
 
